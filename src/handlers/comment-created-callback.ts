e02b02f2a16d748afe19305849fc1b333ce76dc4
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-10-28 17:18:53 +0000

    chore: merge fix and format

--- src/handlers/comment-created-callback.ts
+++ src/handlers/comment-created-callback.ts
@@ -2,7 +2,8 @@ import { Context, SupportedEvents } from "../types";
 import { addCommentToIssue } from "./add-comment";
 import { askQuestion } from "./ask-llm";
 import { CallbackResult } from "../types/proxy";
-import { bubbleUpErrorComment } from "../helpers/errors";
+import { bubbleUpErrorComment, sanitizeMetadata } from "../helpers/errors";
+import { LogReturn } from "@ubiquity-os/ubiquity-os-logger";
 
 export async function issueCommentCreatedCallback(
   context: Context<"issue_comment.created", SupportedEvents["issue_comment.created"]>
@@ -27,19 +28,52 @@ export async function issueCommentCreatedCallback(
   }
 
   logger.info(`Asking question: ${question}`);
-
+  let commentToPost;
   try {
     const response = await askQuestion(context, question);
-    const { answer, tokenUsage } = response;
+    const { answer, tokenUsage, groundTruths } = response;
     if (!answer) {
       throw logger.error(`No answer from OpenAI`);
     }
     logger.info(`Answer: ${answer}`, { tokenUsage });
-    const tokens = `\n\n<!--\n${JSON.stringify(tokenUsage, null, 2)}\n--!>`;
-    const commentToPost = answer + tokens;
+
+    const metadata = {
+      groundTruths,
+      tokenUsage,
+    };
+
+    const metadataString = createStructuredMetadata("LLM Ground Truths and Token Usage", logger.info(`Answer: ${answer}`, { metadata }));
+    commentToPost = answer + metadataString;
     await addCommentToIssue(context, commentToPost);
     return { status: 200, reason: logger.info("Comment posted successfully").logMessage.raw };
   } catch (error) {
-    throw await bubbleUpErrorComment(context, error, false);
+    throw await bubbleUpErrorComment(context, error);
   }
+}
+
+function createStructuredMetadata(header: string | undefined, logReturn: LogReturn) {
+  let logMessage, metadata;
+  if (logReturn) {
+    logMessage = logReturn.logMessage;
+    metadata = logReturn.metadata;
+  }
+
+  const jsonPretty = sanitizeMetadata(metadata);
+  const stackLine = new Error().stack?.split("\n")[2] ?? "";
+  const caller = stackLine.match(/at (\S+)/)?.[1] ?? "";
+  const ubiquityMetadataHeader = `\n\n<!-- Ubiquity - ${header} - ${caller} - ${metadata?.revision}`;
+
+  let metadataSerialized: string;
+  const metadataSerializedVisible = ["```json", jsonPretty, "```"].join("\n");
+  const metadataSerializedHidden = [ubiquityMetadataHeader, jsonPretty, "-->"].join("\n");
+
+  if (logMessage?.type === "fatal") {
+    // if the log message is fatal, then we want to show the metadata
+    metadataSerialized = [metadataSerializedVisible, metadataSerializedHidden].join("\n");
+  } else {
+    // otherwise we want to hide it
+    metadataSerialized = metadataSerializedHidden;
+  }
+
+  return metadataSerialized;
 }

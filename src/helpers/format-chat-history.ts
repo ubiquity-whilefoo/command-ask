34b7ab8d8b78d5abc349f69014884c27676b42a7
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-10-31 21:27:25 +0000

    chore: token handling

--- src/helpers/format-chat-history.ts
+++ src/helpers/format-chat-history.ts
@@ -1,8 +1,9 @@
 import { Context } from "../types";
-import { StreamlinedComment, StreamlinedComments } from "../types/llm";
+import { StreamlinedComment, StreamlinedComments, TokenLimits } from "../types/llm";
 import { createKey, streamlineComments } from "../handlers/comments";
 import { fetchPullRequestDiff, fetchIssue, fetchIssueComments } from "./issue-fetching";
 import { pullReadmeFromRepoForIssue, splitKey } from "./issue";
+import { logger } from "./errors";
 
 export async function formatChatHistory(
   context: Context,
@@ -10,24 +11,37 @@ export async function formatChatHistory(
   specAndBodies: Record<string, string>
 ): Promise<string[]> {
   const keys = new Set([...Object.keys(streamlined), ...Object.keys(specAndBodies), createKey(context.payload.issue.html_url)]);
-  let runningTokenCount = 0;
+  const tokenLimits: TokenLimits = {
+    modelMaxTokenLimit: context.adapters.openai.completions.getModelMaxTokenLimit(context.config.model),
+    maxCompletionTokens: context.config.maxTokens || context.adapters.openai.completions.getModelMaxOutputLimit(context.config.model),
+    runningTokenCount: 0,
+    tokensRemaining: 0,
+  };
+
+  // minus the output tokens we have this many tokens to use
+  tokenLimits.tokensRemaining = tokenLimits.modelMaxTokenLimit - tokenLimits.maxCompletionTokens;
 
   const chatHistory = await Promise.all(
-    Array.from(keys).map(async (key) => {
+    Array.from(keys).map(async (key, i) => {
+      if (tokenLimits.tokensRemaining < 0) {
+        logger.error(`Ran out of tokens at block ${i}`);
+        return "";
+      }
       const [currentTokenCount, result] = await createContextBlockSection({
         context,
         key,
         streamlined,
         specAndBodies,
         isCurrentIssue: key === createKey(context.payload.issue.html_url),
-        currentContextTokenCount: runningTokenCount,
+        tokenLimits,
       });
-      runningTokenCount += currentTokenCount;
+      tokenLimits.runningTokenCount = currentTokenCount;
+      tokenLimits.tokensRemaining = tokenLimits.modelMaxTokenLimit - tokenLimits.maxCompletionTokens - currentTokenCount;
       return result;
     })
   );
 
-  return Array.from(new Set(chatHistory));
+  return Array.from(new Set(chatHistory)).filter((x): x is string => !!x);
 }
 
 function getCorrectHeaderString(prDiff: string | null, isCurrentIssue: boolean, isConvo: boolean) {
@@ -66,14 +80,14 @@ async function createContextBlockSection({
   streamlined,
   specAndBodies,
   isCurrentIssue,
-  currentContextTokenCount,
+  tokenLimits,
 }: {
   context: Context;
   key: string;
   streamlined: Record<string, StreamlinedComment[]>;
   specAndBodies: Record<string, string>;
   isCurrentIssue: boolean;
-  currentContextTokenCount: number;
+  tokenLimits: TokenLimits;
 }): Promise<[number, string]> {
   let comments = streamlined[key];
   if (!comments || comments.length === 0) {
@@ -94,8 +108,7 @@ async function createContextBlockSection({
     throw context.logger.error("Issue number is not valid");
   }
 
-  const prDiff = await fetchPullRequestDiff(context, org, repo, issueNumber);
-
+  const { diff } = await fetchPullRequestDiff(context, org, repo, issueNumber, tokenLimits);
   let specOrBody = specAndBodies[key];
   if (!specOrBody) {
     specOrBody =
@@ -109,8 +122,8 @@ async function createContextBlockSection({
       )?.body || "No specification or body available";
   }
 
-  const specHeader = getCorrectHeaderString(prDiff, isCurrentIssue, false);
-  const blockHeader = getCorrectHeaderString(prDiff, isCurrentIssue, true);
+  const specHeader = getCorrectHeaderString(diff, isCurrentIssue, false);
+  const blockHeader = getCorrectHeaderString(diff, isCurrentIssue, true);
 
   const specBlock = [createHeader(specHeader, key), createSpecOrBody(specOrBody), createFooter(specHeader, key)];
   const commentSection = createComment({ issueNumber, repo, org, comments }, specOrBody);
@@ -132,15 +145,25 @@ async function createContextBlockSection({
     }
   }
 
-  if (!prDiff) {
-    currentContextTokenCount += await context.adapters.openai.completions.findTokenLength(block.join(""));
-    return [currentContextTokenCount, block.join("\n")];
+  if (!diff) {
+    return [await context.adapters.openai.completions.findTokenLength(block.join("")), block.join("\n")];
   }
 
-  const blockWithDiff = [block.join("\n"), createHeader(`Pull Request Diff`, key), prDiff, createFooter(`Pull Request Diff`, key)];
-  currentContextTokenCount += await context.adapters.openai.completions.findTokenLength(blockWithDiff.join(""));
-  return [currentContextTokenCount, blockWithDiff.join("\n")];
+  const blockWithDiff = [block.join("\n"), createHeader(`Pull Request Diff`, key), diff, createFooter(`Pull Request Diff`, key)];
+  return [await context.adapters.openai.completions.findTokenLength(blockWithDiff.join("")), blockWithDiff.join("\n")];
 }
+
+/**
+ * Might not need to splice from the formatted window
+function removeSections(fullText: string, header: string, footer: string): string {
+  const regex = new RegExp(`${escapeRegExp(header)}[\\s\\S]*?${escapeRegExp(footer)}`, 'g');
+  return fullText.replace(regex, '').trim();
+}
+
+function escapeRegExp(text: string): string {
+  return text.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+ */
 
 function createHeader(content: string, repoString: string) {
   return `=== ${content} === ${repoString} ===\n`;

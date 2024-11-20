cca9decc336b9c88cc71efd0a7231e6d122c6aa7
whilefoo <whilefoo@users.noreply.github.com>
2024-11-20 16:48:05 +0100

    fix: manifest

--- src/worker.ts
+++ src/worker.ts
@@ -7,6 +7,8 @@ import { PluginSettings, pluginSettingsSchema } from "./types/plugin-inputs";
 import manifest from "../manifest.json";
 import { Command } from "./types/command";
 import { plugin } from "./plugin";
+import { Manifest } from "@ubiquity-os/plugin-sdk/manifest";
+import { LogLevel } from "@ubiquity-os/ubiquity-os-logger";
 
 export default {
   async fetch(request: Request, env: Env, executionCtx?: ExecutionContext) {
@@ -17,13 +19,12 @@ export default {
           adapters: {} as ReturnType<typeof createAdapters>,
         });
       },
-      // @ts-expect-error incorrect types
-      manifest,
+      manifest as Manifest,
       {
         envSchema: envSchema,
         postCommentOnError: true,
         settingsSchema: pluginSettingsSchema,
-        logLevel: env.LOG_LEVEL,
+        logLevel: env.LOG_LEVEL as LogLevel,
         kernelPublicKey: env.KERNEL_PUBLIC_KEY,
       }
     ).fetch(request, env, executionCtx);

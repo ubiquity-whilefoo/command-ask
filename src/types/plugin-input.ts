1f99cd515fe9f8a2ee52c0d948134ec423c3b18c
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-11-26 12:36:01 +0000

    fix(config): add descriptions to JSON schema properties

--- src/types/plugin-input.ts
+++ src/types/plugin-input.ts
@@ -9,10 +9,10 @@ import { StaticDecode, Type as T } from "@sinclair/typebox";
  */
 
 export const pluginSettingsSchema = T.Object({
-  model: T.String({ default: "o1-mini" }),
-  openAiBaseUrl: T.Optional(T.String()),
-  similarityThreshold: T.Number({ default: 0.9 }),
-  maxTokens: T.Number({ default: 10000 }),
+  model: T.String({ default: "o1-mini", description: "The LLM model you wish to use" }),
+  openAiBaseUrl: T.Optional(T.String({ description: "The base URL for the OpenAI API" })),
+  similarityThreshold: T.Number({ default: 0.9, description: "When fetching embeddings context, the similarity threshold to use (1- similarityThreshold)" }),
+  maxTokens: T.Number({ default: 10000, description: "The max completion tokens you want to the model to generate" }),
 });
 
 export type PluginSettings = StaticDecode<typeof pluginSettingsSchema>;

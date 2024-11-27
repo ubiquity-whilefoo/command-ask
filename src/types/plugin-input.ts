4b9bf237aafad514597cd5f3b374b99eb18255b8
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-11-27 19:52:23 +0000

    chore: config examples

--- src/types/plugin-input.ts
+++ src/types/plugin-input.ts
@@ -9,8 +9,15 @@ import { StaticDecode, Type as T } from "@sinclair/typebox";
  */
 
 export const pluginSettingsSchema = T.Object({
-  model: T.String({ default: "o1-mini", description: "The LLM model you wish to use" }),
-  openAiBaseUrl: T.Optional(T.String({ description: "The base URL for the OpenAI API" })),
+  model: T.String({
+    default: "o1-mini",
+    description: "The LLM model you wish to use",
+    examples: ["openai/gpt-4o", "openai/o1-mini"]
+  }),
+  openAiBaseUrl: T.Optional(T.String({
+    description: "The base URL for the OpenAI API",
+    examples: ["https://openrouter.ai/api/v1", "https://api.openai.com/v1"]
+  })),
   similarityThreshold: T.Number({ default: 0.9, description: "When fetching embeddings context, the similarity threshold to use (1- similarityThreshold)" }),
   maxTokens: T.Number({ default: 10000, description: "The max completion tokens you want to the model to generate" }),
 });
